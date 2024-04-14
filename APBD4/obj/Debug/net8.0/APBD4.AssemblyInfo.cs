b39d5be103696c633e7a13eb7243da2714b4fb4c
s28403 <s28403@pjwstk.edu.pl>
2024-04-14 19:00:26 +0200

    done

--- APBD4/obj/Debug/net8.0/APBD4.AssemblyInfo.cs
+++ APBD4/obj/Debug/net8.0/APBD4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4197aca84a229bb948a3eb545d11c4b8145219a")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
